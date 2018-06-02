7e264752f6948c81c6edf72dd7c6212e0518040f
Marc Wang <474733787@qq.com>
2018-06-02 10:12:35 +0800

    diagnose

--- train.py
+++ train.py
@@ -131,6 +131,7 @@ def test_batch(net, embs, hid2out, X, Y):
 
     ncorrect = 0
     ntotal = 0
+    outputs=outputs.cpu()
     for seq_i in range(batch_size):
         for w_i in range(outp_seq_len):
             if outputs[w_i,seq_i]!=Y[w_i,seq_i]:
